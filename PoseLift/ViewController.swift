15dc89f86b87351266a3ea023839756ecfc18ed1
hkair <hb2kang@gmail.com>
2020-10-15 22:20:07 -0400

    Initial Commit

---
+++ PoseLift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoseLift
+//
+//  Created by Hobin Kang on 2020-10-15.
+//  Copyright © 2020 hobink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
