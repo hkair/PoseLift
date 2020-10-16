073599e8cd6b716683c460c9b55fd2dea44f7e64
hkair <hb2kang@gmail.com>
2020-10-15 23:32:52 -0400

    Add ImagePickerController and AVPlayer

--- PoseLift/ViewController.swift
+++ PoseLift/ViewController.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
-import CoreML
-import Vision 
+import AVKit
+import AVFoundation
 
-class ViewController: UIViewController {
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var videoView: UIView!
+    
+    // video
+    var videoURL: URL?
+    var player: AVPlayer!
+    var avpController = AVPlayerViewController()
+    
+    // gallery
+    let galleryPicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
+            player = AVPlayer(url: videoURL)
+            avpController = AVPlayerViewController()
+            avpController.player = player
+            avpController.view.frame = videoView.frame
+            self.addChild(avpController)
+            self.view.addSubview(avpController.view)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTouched(_ sender: UIBarButtonItem) {
+        galleryPicker.sourceType = .photoLibrary
+        galleryPicker.delegate = self
+        galleryPicker.mediaTypes = ["public.image", "public.movie"]
+        present(galleryPicker, animated: true, completion: nil)
+    }
+    
 }
 
